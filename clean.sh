f274e86fd799feafcbfb3ef2899025625db614f7
otman oulcaid <57399454+otmanoulcaid@users.noreply.github.com>
2025-02-15 02:14:55 +0100

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -2,5 +2,5 @@
 
 CONTAINER_NAME=$1
 
-(docker stop $CONTAINER_NAME && echo "Stopping old container: $CONTAINER_NAME")  2> /dev/null
-(docker rm $CONTAINER_NAME && echo "Removing old container: $CONTAINER_NAME") 2> /dev/null
+(docker stop $CONTAINER_NAME && echo "Stopping old container: $CONTAINER_NAME")  2> /dev/null || true
+(docker rm $CONTAINER_NAME && echo "Removing old container: $CONTAINER_NAME") 2> /dev/null || true
